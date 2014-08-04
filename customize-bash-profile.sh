dcedc0940405fbc35a4d2130179c9732a76c606a
Shawn Woodford <shawn@kingandpartners.com>
2014-08-04 17:29:28 -0400

    add some more things

--- customize-bash-profile.sh
+++ customize-bash-profile.sh
@@ -7,10 +7,8 @@ curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-pro
 # Load in the git branch prompt script in your bash profile.
 echo "source ~/.git-prompt.sh" >> ~/.bash_profile
 
-# Copy this into your .bash_profile
-# export PS1="\h\[\e[32;1m\] \w\[\e[32;1m\] \$(__git_ps1) \[\e[0m\]"
+# Setup custom command prompt in your .bash_profile
 echo 'export PS1="\h\[\e[32;1m\] \w\[\e[32;1m\] \$(__git_ps1) \[\e[0m\]"' >> ~/.bash_profile
-
 
 # Autocomplete Git Commands and Branch Names in Bash
 # http://code-worrier.com/blog/autocomplete-git/
@@ -30,3 +28,16 @@ export LSCOLORS=eafxcxdxbxegedabagacad" >> ~/.bash_profile
 # Tell grep to highlight matches
 # export GREP_OPTIONS='--color=auto'
 echo "export GREP_OPTIONS='--color=auto'" >> ~/.bash_profile
+
+# Add alias for ll
+echo "alias ll="ls -fl"" >> ~/.bash_profile
+
+# Source in .profile
+echo "source ~/.profile" >> ~/.bash_profile
+
+# Load RVM into a shell session *as a function*
+echo "[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"" >> ~/.bash_profile
+
+# Set Java HOME
+echo "export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home" >> ~/.bash_profile
+
