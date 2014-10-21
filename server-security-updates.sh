8e99efd895ac189ebf76aa5ab4e7590b26877a3f
Shawn Woodford <shawn@kingandpartners.com>
2014-10-21 14:53:22 -0400

    install updates in the background, 1000x faster!

--- server-security-updates.sh
+++ server-security-updates.sh
@@ -3,11 +3,12 @@
 # This script will read from the list of servers in the file serverupdatelist, 
 # ssh into each server in the list and run the command specified in the script serverupdates.sh
 
+
 while read serverupdatelist
 do
   scp serverupdates.sh $serverupdatelist:~/
   echo Installing Updates on $serverupdatelist
-  ssh -n $serverupdatelist './serverupdates.sh' #>> serverupdates.log
+  ssh -n $serverupdatelist '(./serverupdates.sh &)' #>> serverupdates.log
   # need to test for success/failure here
   echo $serverupdatelist Updated
 done < serverupdatelist
