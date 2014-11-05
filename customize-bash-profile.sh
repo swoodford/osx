3f09a296a042682350cfe4018200a26b789b6632
Shawn Woodford <shawn@kingandpartners.com>
2014-11-05 16:50:47 -0500

    major refactor

--- customize-bash-profile.sh
+++ customize-bash-profile.sh
@@ -2,45 +2,52 @@
 
 # Bash profile customizations
 
-# Put Your Git Branch in Your Bash Prompt
-# http://code-worrier.com/blog/git-branch-in-bash-prompt/
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+read -rp "This script will add extra features to customize your bash profile, proceed ? (y/n) " CONTINUE
+if [[ $CONTINUE =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	touch ~/.bash_profile
 
-# Load in the git branch prompt script in your bash profile.
-echo "source ~/.git-prompt.sh" >> ~/.bash_profile
+	# Setup custom command prompt in your .bash_profile
+	echo 'export PS1="\h\[\e[32;1m\] \w\[\e[32;1m\] \$(__git_ps1) \[\e[0m\]"' >> ~/.bash_profile
 
-# Setup custom command prompt in your .bash_profile
-echo 'export PS1="\h\[\e[32;1m\] \w\[\e[32;1m\] \$(__git_ps1) \[\e[0m\]"' >> ~/.bash_profile
+	# Put Your Git Branch in Your Bash Prompt
+	# http://code-worrier.com/blog/git-branch-in-bash-prompt/
+	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 
-# Autocomplete Git Commands and Branch Names in Bash
-# http://code-worrier.com/blog/autocomplete-git/
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+	# Load in the git branch prompt script in your bash profile.
+	echo "if [ -f ~/.git-prompt.sh ]; then" >> ~/.bash_profile
+	echo "	source ~/.git-prompt.sh" >> ~/.bash_profile
+	echo "fi" >> ~/.bash_profile
 
-# Add the following lines to your .bash_profile
-echo "if [ -f ~/.git-completion.bash ]; then
-  . ~/.git-completion.bash
-fi" >> ~/.bash_profile
+	# Autocomplete Git Commands and Branch Names in Bash
+	# http://code-worrier.com/blog/autocomplete-git/
+	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
-# Tell ls to be colorful
-# export CLICOLOR=1
-# export LSCOLORS=eafxcxdxbxegedabagacad
-echo "export CLICOLOR=1
-export LSCOLORS=eafxcxdxbxegedabagacad" >> ~/.bash_profile
+	echo "if [ -f ~/.git-completion.bash ]; then" >> ~/.bash_profile
+	echo "	. ~/.git-completion.bash" >> ~/.bash_profile
+	echo "fi" >> ~/.bash_profile
 
-# Tell grep to highlight matches
-# export GREP_OPTIONS='--color=auto'
-echo "export GREP_OPTIONS='--color=auto'" >> ~/.bash_profile
+	# Tell ls to be colorful
+	echo "export CLICOLOR=1" >> ~/.bash_profile
+	echo "export LSCOLORS=eafxcxdxbxegedabagacad" >> ~/.bash_profile
 
-# Add alias for ll
-echo "alias ll='ls -fl'" >> ~/.bash_profile
+	# Tell grep to highlight matches
+	echo "export GREP_OPTIONS='--color=auto'" >> ~/.bash_profile
 
-# Source in .profile
-if [ -f ~/.profile ]; then
-	echo "source ~/.profile" >> ~/.bash_profile
+	# Add alias for ll
+	echo "alias ll='ls -fl'" >> ~/.bash_profile
+
+	# Source in .profile
+	if [ -f ~/.profile ]; then
+		echo "source ~/.profile" >> ~/.bash_profile
+	fi
+
+	# Set Java HOME
+	if ! grep -q "JAVA_HOME" ~/.bash_profile; then
+		echo "export JAVA_HOME=`/usr/libexec/java_home -v1.8`" >> ~/.bash_profile
+	fi
+
+	if ! grep -q "PATH" ~/.bash_profile; then
+		echo "export PATH=/usr/local/bin:\$PATH" >> ~/.bash_profile
+	fi
+
 fi
-
-# Load RVM into a shell session *as a function*
-echo "[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"" >> ~/.bash_profile
-
-# Set Java HOME
-echo "export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home" >> ~/.bash_profile
--- setup-developer-environment.sh
+++ setup-developer-environment.sh
@@ -1,21 +1,68 @@
 #!/bin/bash
-# This script will setup a new developer environment in OS X, it is untested and considered experimental. Proceed carefully!
+# This script will setup a new developer environment in OS X. Proceed carefully!
+# Assumptions: Fresh OS X install, nothing else installed/configured
 
 function pause(){
-	read -p "Press any key to continue..."
+	# read -p "Press any key to continue..."
+	echo ""
 }
 
 function homebrew(){
 	brew install $*
 }
 
-read -rp "This script will setup a new developer environment in OS X, it is untested and considered experimental. Proceed with installation? (y/n) " CONTINUE
+read -rp "This script will setup a new developer environment in OS X, Proceed with installation? (y/n) " CONTINUE
 if [[ $CONTINUE =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
-	echo "Show Hidden Files (Mavericks)"
-	defaults write com.apple.finder AppleShowAllFiles YES
+	echo "Show Hidden Files (Mavericks/Yosemite)"
+	defaults write com.apple.finder AppleShowAllFiles -boolean false ; killall Finder
 
-	echo "Install Xcode (unreliable)"
+	if ! -f ~/.bash_profile; then
+		# Bash profile customizations
+		touch ~/.bash_profile
+		
+		# Setup custom command prompt in your .bash_profile
+		echo 'export PS1="\h\[\e[32;1m\] \w\[\e[32;1m\] \$(__git_ps1) \[\e[0m\]"' >> ~/.bash_profile
+
+		# Put Your Git Branch in Your Bash Prompt
+		# http://code-worrier.com/blog/git-branch-in-bash-prompt/
+		curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+
+		# Load in the git branch prompt script in your bash profile.
+		echo "if [ -f ~/.git-prompt.sh ]; then" >> ~/.bash_profile
+		echo "	source ~/.git-prompt.sh" >> ~/.bash_profile
+		echo "fi" >> ~/.bash_profile
+
+		# Autocomplete Git Commands and Branch Names in Bash
+		# http://code-worrier.com/blog/autocomplete-git/
+		curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+
+		# Add the following lines to your .bash_profile
+		echo "if [ -f ~/.git-completion.bash ]; then" >> ~/.bash_profile
+		echo "	. ~/.git-completion.bash" >> ~/.bash_profile
+		echo "fi" >> ~/.bash_profile
+
+		# Tell ls to be colorful
+		echo "export CLICOLOR=1" >> ~/.bash_profile
+		echo "export LSCOLORS=eafxcxdxbxegedabagacad" >> ~/.bash_profile
+
+		# Tell grep to highlight matches
+		echo "export GREP_OPTIONS='--color=auto'" >> ~/.bash_profile
+
+		# Add alias for ll
+		echo "alias ll='ls -fl'" >> ~/.bash_profile
+
+		# Source in .profile
+		if [ -f ~/.profile ]; then
+			echo "source ~/.profile" >> ~/.bash_profile
+		fi
+
+		echo "Make sure to prepend '/usr/local/bin:' to very beginning of PATH statement in ~/.bash_profile"
+		
+		echo "export PATH=/usr/local/bin:\$PATH" >> ~/.bash_profile
+	fi
+
+	echo "Install Xcode command line tools"
 	xcode-select --install
 	pause
 
@@ -23,61 +70,63 @@ if [[ $CONTINUE =~ ^([yY][eE][sS]|[yY])$ ]]; then
 	open "https://developer.apple.com/downloads/index.action"
 	pause
 
-	read -rp "Warning: Do not proceed until Xcode has been installed. Has Xcode finished installation? (y/n) " XCODE
+	read -rp "Warning: Do not proceed until Xcode command line tools have been installed. Continue? (y/n) " XCODE
 	if [[ $XCODE =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
 		echo "Install Homebrew"
 		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
+		brew update
 		brew doctor
 
-		echo "Make sure to prepend '/usr/local/bin:' to very beginning of PATH statement in ~/.bash_profile"
-		pause
-		echo "export PATH=/usr/local/bin:$PATH" >> ~/.bash_profile
-
 		echo "Install updated git"
-
 		homebrew 'git'
 
 		git --version
 
-		echo "Version should be ≥ git version 2.0.4, if not check $PATH or restart terminal"
+		echo "Version should be ≥ git version 2.1.3, if not check $PATH or restart terminal"
 		pause
 
 		echo "Speed up gem installation by disabling documentation"
-		echo "gem: --no-document" >> ~/.gemrc
+		if ! -f ~/.gemrc; then
+			touch ~/.gemrc
+		fi
 
-		echo "Install RVM with Ruby 1.9.3"
+		if ! grep -q "no-document" ~/.gemrc; then
+			echo "gem: --no-document" >> ~/.gemrc
+		fi
 
-		curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3
+		source ~/.bash_profile
+		
+		read -rp "Install RVM with Ruby, which Ruby version? (ex. 1.9.3, 2.0.0) " RUBYV
+
+		gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+
+		curl -sSL https://get.rvm.io | bash -s stable --ruby=$RUBYV
 
 		source ~/.rvm/scripts/rvm
 
+		echo "Update RVM"
+		rvm get stable
+
+		echo "Cleanup RVM"
+		rvm cleanup all
+
 		echo "Set default global ruby version"
 
-		rvm use 1.9.3 --default
+		rvm use $RUBYV --default
 
 		echo "Set local ruby version"
 
-		rvm use 1.9.3
+		rvm use $RUBYV
 
 		ruby -v
 
-		echo "Should be ≥ ruby 1.9.3p547"
+		# echo "Should be ≥ ruby 1.9.3p547"
 		pause
 
 		which ruby
 
-		echo "Should show ~/.rvm/rubies/ruby-1.9.3-p547/bin/ruby"
-		pause
-
-		echo "Update RubyGems"
-
-		gem update --system
-
-		gem -v
-
-		echo "Should be ≥ 2.4.1"
+		echo "Should show ~/.rvm/rubies/ruby-x.y.z.version/bin/ruby"
 		pause
 
 		echo "Install Rails"
@@ -86,7 +135,16 @@ if [[ $CONTINUE =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
 		rails -v
 
-		echo "Should be ≥ Rails 4.1.4"
+		echo "Should be ≥ Rails 4.1.7"
+		pause
+
+		echo "Update RubyGems"
+
+		gem update --system
+
+		gem -v
+
+		echo "Should be ≥ 2.4.2"
 		pause
 
 		echo "Install extra brew packages"
@@ -99,26 +157,40 @@ if [[ $CONTINUE =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
 		homebrew 'python'
 
+		python --version
+
 		echo "python --version Should be ≥ Python 2.7.8, if not check $PATH or restart terminal"
+		pause
+
+		echo "Check Java version"
+
+		java -version
 
 		echo "Install Java JDK 8"
 
 		open "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
 		pause
 
-		echo "Set JAVA_HOME"
-		echo "export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home" >> ~/.bash_profile
+		read -rp "Warning: Do not proceed until Java JDK has been installed. Continue? (y/n) " JAVA
+		if [[ $JAVA =~ ^([yY][eE][sS]|[yY])$ ]]; then
+			echo "Set JAVA_HOME"
+
+			if ! grep -q "JAVA_HOME" ~/.bash_profile; then
+				echo "export JAVA_HOME=`/usr/libexec/java_home -v1.8`" >> ~/.bash_profile
+			fi
+		fi
 
 		echo "Install s3cmd"
-		wget http://downloads.sourceforge.net/project/s3tools/s3cmd/1.5.0-beta1/s3cmd-1.5.0-beta1.tar.gz
-		tar xzf s3cmd-1.5.0-beta1.tar.gz
-		cd s3cmd-1.5.0-beta1
-		python setup.py install
-
-		read -rp "Configure s3cmd? (y/n) " CONFIGURE
-		if [[ $CONFIGURE =~ ^([yY][eE][sS]|[yY])$ ]]; then
-			s3cmd --configure
-		fi
+		command -v s3cmd >/dev/null 2>&1 || {
+			cd ~
+			git clone https://github.com/s3tools/s3cmd.git
+			cd s3cmd
+			sudo python setup.py install
+			read -rp "Configure s3cmd? (y/n) " CONFIGURE
+			if [[ $CONFIGURE =~ ^([yY][eE][sS]|[yY])$ ]]; then
+				s3cmd --configure
+			fi
+		}
 
 		echo "Install AWS CLI"
 		pip install awscli
@@ -136,4 +208,3 @@ else
 	echo "Cancelled."
 	exit 1
 fi
-
