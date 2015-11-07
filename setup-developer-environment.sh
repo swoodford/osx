dc5cb1d65393aee947712f70c3909cd523994191
Shawn Woodford <sw@shawnwoodford.com>
2015-11-07 12:04:41 -0500

    Updates

--- setup-developer-environment.sh
+++ setup-developer-environment.sh
@@ -14,7 +14,7 @@ function homebrew(){
 read -rp "This script will setup a new developer environment in OS X, Proceed with installation? (y/n) " CONTINUE
 if [[ $CONTINUE =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
-	echo "Show Hidden Files (Mavericks/Yosemite)"
+	echo "Show Hidden Files (Mavericks/Yosemite/El Capitan)"
 	defaults write com.apple.finder AppleShowAllFiles -boolean true ; killall Finder
 	# defaults write com.apple.finder AppleShowAllFiles YES && killall Finder
 
@@ -107,68 +107,74 @@ if [[ $CONTINUE =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
 		git --version
 
-		echo "Version should be ≥ git version 2.2.2, if not check $PATH or restart terminal"
+		echo "Version should be ≥ git version 2.6.3, if not check $PATH or restart terminal"
 		pause
 
-		echo "Speed up gem installation by disabling documentation"
-		if ! -f ~/.gemrc; then
-			touch ~/.gemrc
+
+		read -rp "Install Ruby on Rails environment? (y/n) " RUBY
+		if [[ $RUBY =~ ^([yY][eE][sS]|[yY])$ ]]; then
+
+
+			echo "Speed up gem installation by disabling documentation"
+			if ! -f ~/.gemrc; then
+				touch ~/.gemrc
+			fi
+
+			if ! grep -q "no-document" ~/.gemrc; then
+				echo "gem: --no-document" >> ~/.gemrc
+			fi
+
+			read -rp "Install RVM with Ruby, which Ruby version? (ex. 1.9.3, 2.0.0) " RUBYV
+
+			gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+
+			curl -sSL https://get.rvm.io | bash -s stable --auto-dotfiles --autolibs=enable --ruby=$RUBYV
+
+			source ~/.rvm/scripts/rvm
+			source ~/.bash_profile
+
+			echo "Update RVM"
+			rvm get stable
+
+			echo "Cleanup RVM"
+			rvm cleanup all
+
+			echo "Set default global ruby version"
+
+			rvm use $RUBYV --default
+
+			echo "Set local ruby version"
+
+			rvm use $RUBYV
+
+			ruby -v
+
+			# echo "Should be ≥ ruby 1.9.3p547"
+			pause
+
+			which ruby
+
+			echo "Should show ~/.rvm/rubies/ruby-x.y.z.version/bin/ruby"
+			pause
+
+			echo "Install Rails"
+
+			gem install rails
+
+			rails -v
+
+			echo "Should be ≥ Rails 4.2.0"
+			pause
+
+			echo "Update RubyGems"
+
+			gem update --system
+
+			gem -v
+
+			echo "Should be ≥ RubyGems 2.4.5"
+			pause
 		fi
-
-		if ! grep -q "no-document" ~/.gemrc; then
-			echo "gem: --no-document" >> ~/.gemrc
-		fi
-		
-		read -rp "Install RVM with Ruby, which Ruby version? (ex. 1.9.3, 2.0.0) " RUBYV
-
-		gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-
-		curl -sSL https://get.rvm.io | bash -s stable --auto-dotfiles --autolibs=enable --ruby=$RUBYV
-
-		source ~/.rvm/scripts/rvm
-		source ~/.bash_profile
-
-		echo "Update RVM"
-		rvm get stable
-
-		echo "Cleanup RVM"
-		rvm cleanup all
-
-		echo "Set default global ruby version"
-
-		rvm use $RUBYV --default
-
-		echo "Set local ruby version"
-
-		rvm use $RUBYV
-
-		ruby -v
-
-		# echo "Should be ≥ ruby 1.9.3p547"
-		pause
-
-		which ruby
-
-		echo "Should show ~/.rvm/rubies/ruby-x.y.z.version/bin/ruby"
-		pause
-
-		echo "Install Rails"
-
-		gem install rails
-
-		rails -v
-
-		echo "Should be ≥ Rails 4.2.0"
-		pause
-
-		echo "Update RubyGems"
-
-		gem update --system
-
-		gem -v
-
-		echo "Should be ≥ RubyGems 2.4.5"
-		pause
 
 		echo "Install extra brew packages"
 
@@ -189,7 +195,7 @@ if [[ $CONTINUE =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
 		python --version
 
-		echo "python --version Should be ≥ Python 2.7.9, if not check $PATH or restart terminal"
+		echo "python --version Should be ≥ Python 2.7.10, if not check $PATH or restart terminal"
 		pause
 
 		echo "Check Java version"
@@ -206,7 +212,7 @@ if [[ $CONTINUE =~ ^([yY][eE][sS]|[yY])$ ]]; then
 			echo "Set JAVA_HOME"
 
 			if ! grep -q "JAVA_HOME" ~/.bash_profile; then
-				echo "export JAVA_HOME=`/usr/libexec/java_home -v1.8`" >> ~/.bash_profile
+				echo 'export JAVA_HOME=$(/usr/libexec/java_home)' >> ~/.bash_profile
 			fi
 		fi
 
